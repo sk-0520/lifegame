3fa9c71a74873f73eb8ada076a4c3755013aefc6
sk <content.type.text.net@gmail.com>
2023-06-21 20:34:41 +0900

    main は最小限にしとく

---
+++ src/entry.rs
@@ -0,0 +1,21 @@
+use crate::project::output_setting::OutputSetting;
+use crate::project::project_options::ProjectOptions;
+use crate::project::input_setting::InputSetting;
+
+pub fn work() {
+    let options = ProjectOptions {
+        input: InputSetting {
+            directory: String::from("X:\\lhh\\project"),
+        },
+        output: OutputSetting {
+            directory: String::from("X:\\lhh\\export"),
+        },
+    };
+    println!("options: {:?}", options);
+
+    let serialized = serde_json::to_string(&options).unwrap();
+    println!("serialized: {}", serialized);
+
+    let deserialized: ProjectOptions = serde_json::from_str(serialized.as_str()).unwrap();
+    println!("deserialized: {:?}", deserialized);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
 extern crate serde_json;
 
+mod entry;
 mod project;
 
-use project::output_setting::OutputSetting;
-use project::project_options::ProjectOptions;
-use project::input_setting::InputSetting;
-
 fn main() {
-    let options = ProjectOptions {
-        input: InputSetting {
-            directory: String::from("X:\\lhp\\project"),
-        },
-        output: OutputSetting {
-            directory: String::from("X:\\lhp\\export"),
-        },
-    };
-    println!("options: {:?}", options);
-
-    let serialized = serde_json::to_string(&options).unwrap();
-    println!("serialized: {}", serialized);
-
-    let deserialized: ProjectOptions = serde_json::from_str(serialized.as_str()).unwrap();
-    println!("deserialized: {:?}", deserialized);
+    entry::work();
 }
